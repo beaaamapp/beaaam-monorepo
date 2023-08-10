dba048ab30ede491418339754ad6312aadc3e9ab
malawad <50057882+malawadd@users.noreply.github.com>
2023-08-10 11:03:18 +0300

    🏆 sveltekit app init

---
+++ webapp/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess({
+    postcss: true,
+  }),
+
+  kit: {
+    adapter: adapter(),
+  },
+};
+
+export default config;
